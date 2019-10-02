34c0efb1687b9acef6639334830c8d878fe1360b
Mr4d <sford@sd43.bc.ca>
2019-10-02 12:08:07 -0700

    Edits by 4d

--- Sales_Tax_Calculator_AidandeVaal.py
+++ Sales_Tax_Calculator_AidandeVaal.py
@@ -1,6 +1,5 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> tax = 1.12
+
+tax = 1.12
 first_item = float (input ("Price of 1st item: "))
 second_item = float (input("Price of second item: "))
 third_item = float (input("Price of third item: "))
