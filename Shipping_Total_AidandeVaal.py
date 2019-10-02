1f4a1fb5dd34b355b0a0e6e678bd9c327677f1b1
Mr4d <sford@sd43.bc.ca>
2019-10-02 12:12:46 -0700

    Edits and Feedback

--- Shipping_Total_AidandeVaal.py
+++ Shipping_Total_AidandeVaal.py
@@ -1,11 +1,14 @@
+
+'''you need to review data types float vs int. Also thje program should give some feedback by combining strigs and floats'''
+
 item1=input("What is the price of your 1st item? $")
 item2=input("What is the price of your 2nd item? $")
 total = int(item1) + int(item2)
 if float(total) > 49:
-    print int(total)
+    print(int(total))
 else:
     added = int(total) + 10
-    print int(added)
+    print (int(added))
         
 
             
